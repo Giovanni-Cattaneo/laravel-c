14ed2817b7684bfafe5f3fcd0075ade6a9637571
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-05-07 14:36:37 +0200

    init

---
+++ resources/views/layout/layout.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+</head>
+
+<body>
+    <header>
+        <h1>Welcome</h1>
+    </header>
+    <main>
+        @yield('main-content')
+    </main>
+    <footer></footer>
+</body>
+
+</html>
